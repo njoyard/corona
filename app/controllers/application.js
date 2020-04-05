4e36e4f580faed35ac60444dc4b976c78514fd7e
Nicolas Joyard <nicolas@njoyard.fr>
2020-04-05 16:37:54 +0200

    select link on share

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,6 +2,7 @@ import Controller from '@ember/controller'
 import { tracked } from '@glimmer/tracking'
 import { action } from '@ember/object'
 import { inject as service } from '@ember/service'
+import { scheduleOnce } from '@ember/runloop'
 import moment from 'moment'
 import env from 'corona/config/environment'
 import { generateDataset, formatYTick } from 'corona/utils/chart'
@@ -133,6 +134,18 @@ export default class ApplicationController extends Controller {
     }
 
     return `${info}.`
+  }
+
+  @action
+  share() {
+    this.showShareDialog = true
+
+    scheduleOnce('afterRender', this, function () {
+      let input = document.querySelector('.share-input input')
+
+      input.focus()
+      input.select()
+    })
   }
 
   @tracked regionFilter = ''
