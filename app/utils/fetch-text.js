bed184510ec44e1758fbe61023dbe1385184f1ac
Nicolas Joyard <nicolas@njoyard.fr>
2020-04-10 14:20:34 +0200

    avoid redownloads in dev

--- app/utils/fetch-text.js
+++ app/utils/fetch-text.js
@@ -1,4 +1,7 @@
 import fetch from 'fetch'
+import env from 'corona/config/environment'
+
+const { environment } = env
 
 const DEFAULT_TIMEOUT = 5000
 const downloaded = new Set()
@@ -10,8 +13,14 @@ export default function fetchText(url, fallbackCallback) {
   let ls = localStorage.getItem(lsKey)
 
   return new Promise((resolve, reject) => {
-    // Avoid redownloading in the same session
-    if (ls && downloaded.has(url)) {
+    // Avoid redownloading in the same session or in dev mode
+    if (ls && (environment === 'development' || downloaded.has(url))) {
+      if (environment === 'development') {
+        console.warn(
+          'Not downloading data in development, clear local storage to force a refresh'
+        )
+      }
+
       return resolve(ls)
     }
 
