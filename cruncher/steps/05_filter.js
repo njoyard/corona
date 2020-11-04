4fdcb774b925904e36f9dbf27813847416205826
Nicolas Joyard <nicolas@njoyard.fr>
2020-11-04 17:49:18 +0100

    cruncher: revert repeat filtering

--- cruncher/steps/05_filter.js
+++ cruncher/steps/05_filter.js
@@ -1,8 +1,5 @@
 import { debugOutput } from '../utils'
 
-// Fields that count new daily events, don't remove repeated values on those
-const DAILY_COUNTERS = new Set(['tests', 'positives'])
-
 export default async function filter(data) {
   console.log('Removing US counties')
 
@@ -16,30 +13,10 @@ export default async function filter(data) {
   for (let zone of data) {
     for (let field of zone.meta.fields) {
       let firstNonZero = zone.points.findIndex((p) => p[field] !== 0)
-      let lastNonZero =
-        zone.points.length -
-        1 -
-        [...zone.points].reverse().findIndex((p) => p[field] !== 0)
 
       // Delete field in all points until the first non zero value
-
       for (let i = 0; i < firstNonZero; i++) {
         delete zone.points[i][field]
-      }
-
-      if (DAILY_COUNTERS.has(field)) {
-        continue
-      }
-
-      // Remove repeated values, except when it's the last point in the series
-      let previousValue = NaN
-      for (let i = 0; i < lastNonZero; i++) {
-        let point = zone.points[i]
-        if (point[field] === previousValue) {
-          delete point[field]
-        } else if (point[field] !== undefined) {
-          previousValue = point[field]
-        }
       }
     }
 
