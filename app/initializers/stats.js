04f8156511748134c983e11fd58f384a38497855
Nicolas Joyard <nicolas@njoyard.fr>
2020-11-06 20:28:30 +0100

    stats for all routes

--- app/initializers/stats.js
+++ app/initializers/stats.js
@@ -4,10 +4,48 @@ const { environment } = config
 
 export function initialize() {
   if (environment === 'production') {
+    window.goatcounter = { no_onload: true }
+
     let script = document.createElement('script')
     script.setAttribute('data-goatcounter', 'https://stats.njoyard.fr/count')
     script.setAttribute('src', '//stats.njoyard.fr/count.js')
     document.body.appendChild(script)
+
+    let ready = false
+    let available = true
+    let pending = location.hash ? [location.pathname + location.hash] : []
+
+    function sendCounts() {
+      while (pending.length) {
+        window.goatcounter.count({ path: pending.shift() })
+      }
+    }
+
+    let readyStart = Date.now()
+    let readyInterval = setInterval(() => {
+      if (window.goatcounter && window.goatcounter.count) {
+        clearInterval(readyInterval)
+        ready = true
+        sendCounts()
+      } else if (Date.now() > readyStart + 20000) {
+        clearInterval(readyInterval)
+        pending = null
+        available = false
+        console.warn('not logging stats: unavailable')
+      }
+    }, 1000)
+
+    window.addEventListener('hashchange', () => {
+      if (available) {
+        pending.push(location.pathname + location.hash)
+
+        if (ready) {
+          sendCounts()
+        }
+      }
+    })
+  } else {
+    console.warn('not logging stats: not in production')
   }
 }
 
